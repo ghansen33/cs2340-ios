1bf3e6ed96e13bf240b93b929d711c669f54716d
Grace Hansen <ghansen33@gatech.edu>
2018-12-03 10:33:03 -0500

    new map with pins

---
+++ cs2340 ios 3/cs2340 ios 3/Pin.swift
@@ -0,0 +1,63 @@
+//
+//  Pin.swift
+//  cs2340 ios 3
+//
+//  Created by Grace Hansen on 12/1/18.
+//  Copyright © 2018 Grace Hansen. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+
+class Pin: NSObject, Codable {
+    let name: String
+    let caption: String
+    let latitude: Double
+    let longitude: Double
+    
+    init(name: String, description: String, latitude: Double, longitude: Double) {
+        self.name = name
+        self.caption = description
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
+
+
+extension Pin {
+    static let pinsKey = "pins"
+    
+    static func save(pins: [Pin]) {
+        let data = try? JSONEncoder().encode(pins)  // json encoding
+        UserDefaults.standard.set(data, forKey: pinsKey)
+    }
+    
+    static func loadPins() -> [Pin]? {
+        if let data = UserDefaults.standard.value(forKey: pinsKey) as? Data,  // UserDefaults
+            let pins = try? JSONDecoder().decode([Pin].self, from: data) {     // json decoding
+            
+            return pins
+        }
+        return nil
+    }
+}
+
+
+// MARK: - MKANNOTATION
+extension Pin: MKAnnotation {
+    
+    // required
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude)
+    }
+    
+    // computed properties: these display the Pin attributes on the map
+    public var title: String? {
+        return self.name
+    }
+    
+    public var subtitle: String? {
+        return self.caption
+    }
+}
